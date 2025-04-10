a46e3b6a01c2189c2378ef89ed965ed324737476
Yungnopw <nopw12345@gmail.com>
2025-04-10 04:57:08 +0200

    remove other files

--- Assets/Scripts/Core/GlobalHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class GlobalHelper
-{
-    public static string GenerateUniqueID(GameObject gameObject)
-    {
-        return $"{gameObject.scene.name}_{gameObject.transform.position.x}_{gameObject.transform.position.y}";
-    }
-}
